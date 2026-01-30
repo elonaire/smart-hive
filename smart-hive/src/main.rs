8037629b0a50f3f8e3f3943826ba687c3ae8404b
Elon Aseneka <elonsantos63@gmail.com>
2026-01-30 05:56:58 +0300

    Try MQTT and Wifi - Empty wifi pass for wokwi

--- smart-hive/src/main.rs
+++ smart-hive/src/main.rs
@@ -21,6 +21,8 @@ use log::*;
 
 const MQTT_BROKER_URL: &str = env!("MQTT_BROKER_URL");
 const MQTT_CLIENT_ID: &str = env!("MQTT_CLIENT_ID");
+const MQTT_USERNAME: &str = env!("MQTT_USERNAME");
+const MQTT_PASSWORD: &str = env!("MQTT_PASSWORD");
 
 fn main() {
     // Initialize ESP-IDF runtime
@@ -74,7 +76,7 @@ fn main() {
 
     let _wifi = wifi_create(&sys_loop, &nvs, modem).unwrap();
 
-    let (mut client, mut conn) = mqtt_create(MQTT_BROKER_URL, MQTT_CLIENT_ID).unwrap();
+    let (mut client, mut conn) = mqtt_create(MQTT_BROKER_URL, MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD).unwrap();
 
     create_event_loop(&mut client, &mut conn, "smart-hive/start-harvest").unwrap();
 }
--- smart-hive/src/mqtt/mqtt.rs
+++ smart-hive/src/mqtt/mqtt.rs
@@ -4,11 +4,15 @@ use esp_idf_svc::sys::EspError;
 pub fn mqtt_create(
     url: &str,
     client_id: &str,
+    username: &str,
+    password: &str,
 ) -> Result<(EspMqttClient<'static>, EspMqttConnection), EspError> {
     let (mqtt_client, mqtt_conn) = EspMqttClient::new(
         url,
         &MqttClientConfiguration {
             client_id: Some(client_id),
+            username: Some(username),
+            password: Some(password),
             ..Default::default()
         },
     )?;
